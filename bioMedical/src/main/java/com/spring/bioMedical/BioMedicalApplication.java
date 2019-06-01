0517aa41b097577c2c7802ccf85d87123578c841
soumyadip007 <soumyadip.note@gmail.com>
2019-06-02 00:30:59 +0530

    Project setup om

---
+++ bioMedical/src/main/java/com/spring/bioMedical/BioMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.spring.bioMedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioMedicalApplication.class, args);
+	}
+
+}
