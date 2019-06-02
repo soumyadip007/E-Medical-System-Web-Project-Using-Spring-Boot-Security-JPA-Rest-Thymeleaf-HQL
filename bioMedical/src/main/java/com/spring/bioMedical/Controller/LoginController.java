0c85dd90c3dde85aec75cd8f049153e836be4c43
soumyadip007 <soumyadip.note@gmail.com>
2019-06-02 16:03:52 +0530

    Login conrtoller added

---
+++ bioMedical/src/main/java/com/spring/bioMedical/Controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.spring.bioMedical.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+}
