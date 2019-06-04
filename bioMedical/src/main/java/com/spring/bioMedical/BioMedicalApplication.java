4b7fa018dc58ff31fd5e1be55e5e7e8109cd4f55
soumyadip007 <soumyadip.note@gmail.com>
2019-06-04 12:07:01 +0530

    POJO bug fixed

--- bioMedical/src/main/java/com/spring/bioMedical/BioMedicalApplication.java
+++ bioMedical/src/main/java/com/spring/bioMedical/BioMedicalApplication.java
@@ -2,8 +2,10 @@ package com.spring.bioMedical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BioMedicalApplication {
 
 	public static void main(String[] args) {
--- bioMedical/src/main/java/com/spring/bioMedical/Controller/RegisterController.java
+++ bioMedical/src/main/java/com/spring/bioMedical/Controller/RegisterController.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
