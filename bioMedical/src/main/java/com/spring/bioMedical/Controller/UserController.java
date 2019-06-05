6444f087ed3559bb7aa640eb1fcf8a870afb3634
soumyadip007 <soumyadip.note@gmail.com>
2019-06-05 14:05:51 +0530

    UserController class added

--- bioMedical/src/main/java/com/spring/bioMedical/Controller/UserController.java
+++ bioMedical/src/main/java/com/spring/bioMedical/Controller/UserController.java
@@ -1,5 +1,18 @@
 package com.spring.bioMedical.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
+	@RequestMapping("/index")
+	public String index(){
+
+		
+		return "user/success";
+	}
+	
+	
 }
