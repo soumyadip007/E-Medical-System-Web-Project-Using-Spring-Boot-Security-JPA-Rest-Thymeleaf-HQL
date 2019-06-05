21e9474c7f6f528b52e95c100f1901fa0df678db
soumyadip007 <soumyadip.note@gmail.com>
2019-06-05 14:33:55 +0530

    @Autowired added

--- bioMedical/src/main/java/com/spring/bioMedical/Controller/AdminController.java
+++ bioMedical/src/main/java/com/spring/bioMedical/Controller/AdminController.java
@@ -1,18 +1,27 @@
 package com.spring.bioMedical.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.bioMedical.service.UserService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+	private UserService userService;
 
-
+	@Autowired
+	public AdminController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	
 	@RequestMapping("/index")
 	public String index(Model model){
-
+		
 		
 		return "admin/success";
 	}
