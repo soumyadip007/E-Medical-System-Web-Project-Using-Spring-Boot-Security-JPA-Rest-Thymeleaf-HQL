6e3cae648dce0e844c6e61d6a1ed0d6c327b505a
soumyadip007 <soumyadip.note@gmail.com>
2019-06-06 14:32:07 +0530

    Admin reconfig

--- bioMedical/src/main/java/com/spring/bioMedical/entity/Admin.java
+++ bioMedical/src/main/java/com/spring/bioMedical/entity/Admin.java
@@ -20,18 +20,22 @@ public class Admin {
 	@Column(name = "id")
 	private int id;
 	
-	@Column(name = "username")
+	@Column(name = "username", nullable = false, unique = true)
+	@Email(message = "Please provide a valid e-mail")
+	@NotEmpty(message = "Please provide an e-mail")
 	private String email;
 	
 	@Column(name = "password")
+	@Transient
 	private String password;
 	
 	@Column(name = "first_name")
+	@NotEmpty(message = "Please provide your first name")
 	private String firstName;
 	
 	@Column(name = "last_name")
+	@NotEmpty(message = "Please provide your last name")
 	private String lastName;
-	
 	
 	@Column(name = "enabled")
 	private boolean enabled;
