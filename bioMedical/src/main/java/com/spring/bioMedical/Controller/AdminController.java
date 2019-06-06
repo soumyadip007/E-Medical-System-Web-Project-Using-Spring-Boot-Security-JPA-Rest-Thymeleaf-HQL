4825426a0b27e6bd453c05a704b5809fe2964ad3
soumyadip007 <soumyadip.note@gmail.com>
2019-06-06 19:40:14 +0530

    add admin added

--- bioMedical/src/main/java/com/spring/bioMedical/Controller/AdminController.java
+++ bioMedical/src/main/java/com/spring/bioMedical/Controller/AdminController.java
@@ -105,4 +105,39 @@ public class AdminController {
 	}
 	
 	
+
+	@GetMapping("/add-doctor")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Admin admin = new Admin();
+		
+		theModel.addAttribute("doctor", admin);
+		
+		return "admin/addDoctor";
+	}
+	
+	
+	@PostMapping("/save-doctor")
+	public String saveEmployee(@ModelAttribute("doctor") Admin admin) {
+		
+		// save the employee
+	//	admin.setId(0);
+		
+		admin.setRole("ROLE_DOCTOR");
+		
+		admin.setPassword("default");
+		
+		admin.setEnabled(true);
+		
+		admin.setConfirmationToken("ByAdmin-Panel");
+		
+		System.out.println(admin);
+		
+		adminServiceImplementation.save(admin);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/admin/userdetails";
+	}
+	
 }
