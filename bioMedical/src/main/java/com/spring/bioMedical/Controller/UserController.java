54d86a8c8a9a7be1170263ba76667b4c20d7ef9c
soumyadip007 <soumyadip.note@gmail.com>
2019-06-07 01:58:20 +0530

    User controller mapping added

--- bioMedical/src/main/java/com/spring/bioMedical/Controller/UserController.java
+++ bioMedical/src/main/java/com/spring/bioMedical/Controller/UserController.java
@@ -1,18 +1,68 @@
 package com.spring.bioMedical.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping("/index")
+	@GetMapping("/index")
 	public String index(){
 
 		
 		return "user/index";
 	}
 	
+
+	@GetMapping("/about")
+	public String about(){
+
+		
+		return "user/about";
+	}
 	
+	@GetMapping("/blog-single")
+	public String bs(){
+
+		
+		return "user/blog-single";
+	}
+	
+	@GetMapping("/blog")
+	public String blog(){
+
+		
+		return "user/blog";
+	}
+	
+	@GetMapping("/contact")
+	public String contact(){
+
+		
+		return "user/contact";
+	}
+	
+
+	@GetMapping("/department-single")
+	public String d(){
+
+		
+		return "user/department-single";
+	}
+
+	@GetMapping("/departments")
+	public String dep(){
+
+		
+		return "user/departments";
+	}
+
+	@GetMapping("/doctor")
+	public String doctor(){
+
+		
+		return "user/doctor";
+	}
 }
