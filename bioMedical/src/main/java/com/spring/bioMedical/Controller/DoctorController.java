f0c6d459a96dc737b552e7a4e7d65a174339f63d
soumyadip007 <soumyadip.note@gmail.com>
2019-06-07 19:40:29 +0530

    Get lastseen of the doctor method merged

--- bioMedical/src/main/java/com/spring/bioMedical/Controller/DoctorController.java
+++ bioMedical/src/main/java/com/spring/bioMedical/Controller/DoctorController.java
@@ -1,9 +1,15 @@
 package com.spring.bioMedical.Controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.bioMedical.entity.Admin;
 import com.spring.bioMedical.service.AdminServiceImplementation;
 import com.spring.bioMedical.service.UserService;
 
@@ -26,6 +32,32 @@ public class DoctorController {
 	@RequestMapping("/index")
 	public String index(){
 
+		// get last seen
+		String username="";
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetails) {
+		   username = ((UserDetails)principal).getUsername();
+		  String Pass = ((UserDetails)principal).getPassword();
+		  System.out.println("One + "+username+"   "+Pass);
+		  
+		  
+		} else {
+		 username = principal.toString();
+		  System.out.println("Two + "+username);
+		}
+		
+		Admin admin = adminServiceImplementation.findByEmail(username);
+				 
+			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+		    Date now = new Date();  
+		    
+		         String log=now.toString();
+		    
+		         admin.setLastseen(log);
+		         
+		         adminServiceImplementation.save(admin);
+		
+		
 		
 		return "doctor/index";
 	}
