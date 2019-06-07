6c17692e06e89f398c07c6349027e175b999624a
soumyadip007 <soumyadip.note@gmail.com>
2019-06-07 19:39:51 +0530

    Doctor controller autowired

--- bioMedical/src/main/java/com/spring/bioMedical/Controller/DoctorController.java
+++ bioMedical/src/main/java/com/spring/bioMedical/Controller/DoctorController.java
@@ -1,12 +1,28 @@
 package com.spring.bioMedical.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.bioMedical.service.AdminServiceImplementation;
+import com.spring.bioMedical.service.UserService;
 
 @Controller
 @RequestMapping("/doctor")
 public class DoctorController {
 
+	private UserService userService;
+
+	private AdminServiceImplementation adminServiceImplementation;
+	
+	@Autowired
+	public DoctorController(UserService userService,AdminServiceImplementation obj ) {
+		this.userService = userService;
+		adminServiceImplementation=obj;
+		 
+	}
+	
+	
 	@RequestMapping("/index")
 	public String index(){
 
