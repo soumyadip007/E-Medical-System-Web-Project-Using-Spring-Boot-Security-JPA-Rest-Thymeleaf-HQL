2b8aeb4e32e2e7002a0ba97eb8273ce3a39caadf
soumyadip007 <soumyadip.note@gmail.com>
2019-06-07 19:33:36 +0530

    Lastseen added to user controller

--- bioMedical/src/main/java/com/spring/bioMedical/Controller/UserController.java
+++ bioMedical/src/main/java/com/spring/bioMedical/Controller/UserController.java
@@ -1,16 +1,51 @@
 package com.spring.bioMedical.Controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.bioMedical.entity.Admin;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	
+	
 	@GetMapping("/index")
 	public String index(){
-
+		
+		// get last seen
+		String username="";
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetails) {
+		   username = ((UserDetails)principal).getUsername();
+		  String Pass = ((UserDetails)principal).getPassword();
+		  System.out.println("One + "+username+"   "+Pass);
+		  
+		  
+		} else {
+		 username = principal.toString();
+		  System.out.println("Two + "+username);
+		}
+		
+		Admin admin = adminServiceImplementation.findByEmail(username);
+				 
+			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+		    Date now = new Date();  
+		    
+		         String log=now.toString();
+		    
+		         admin.setLastseen(log);
+		         
+		         adminServiceImplementation.save(admin);
+		
+		
 		
 		return "user/index";
 	}
