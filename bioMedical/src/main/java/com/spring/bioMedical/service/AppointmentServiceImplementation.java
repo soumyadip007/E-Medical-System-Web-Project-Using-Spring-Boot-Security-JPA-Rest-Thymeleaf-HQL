cf54dffabf9ecb867c3746eceaa8b9c6469dbaa7
soumyadip007 <soumyadip.note@gmail.com>
2019-06-08 18:36:53 +0530

    List<Appointment> findAll() added

--- bioMedical/src/main/java/com/spring/bioMedical/service/AppointmentServiceImplementation.java
+++ bioMedical/src/main/java/com/spring/bioMedical/service/AppointmentServiceImplementation.java
@@ -1,8 +1,11 @@
 package com.spring.bioMedical.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.spring.bioMedical.entity.Admin;
 import com.spring.bioMedical.entity.Appointment;
 import com.spring.bioMedical.repository.AppointmentRepository;
 
@@ -23,6 +26,11 @@ public class AppointmentServiceImplementation  {
 	{
 		
 		appointmentRepository.save(app);
+	}
+	
+	
+	public List<Appointment> findAll() {
+		return appointmentRepository.findAll();
 	}
 
 
