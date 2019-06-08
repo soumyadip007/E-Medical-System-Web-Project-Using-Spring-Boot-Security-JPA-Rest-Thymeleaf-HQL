d2be29067d5ec21338a9977a5fec9cc6254f42c0
soumyadip007 <soumyadip.note@gmail.com>
2019-06-08 17:31:16 +0530

    Repo long -> INT

--- bioMedical/src/main/java/com/spring/bioMedical/repository/AppointmentRepository.java
+++ bioMedical/src/main/java/com/spring/bioMedical/repository/AppointmentRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.spring.bioMedical.entity.Appointment;
 
 @Repository("appointmentRepository")
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
 
 }
