9f43ac46814490c070dad1c33b0ad6b11e7bdb29
soumyadip007 <soumyadip.note@gmail.com>
2019-06-08 15:57:10 +0530

    AppointmentRepository config

--- bioMedical/src/main/java/com/spring/bioMedical/repository/AppointmentRepository.java
+++ bioMedical/src/main/java/com/spring/bioMedical/repository/AppointmentRepository.java
@@ -1,17 +1,11 @@
 package com.spring.bioMedical.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.spring.bioMedical.entity.Admin;
-import com.spring.bioMedical.entity.User;
+import com.spring.bioMedical.entity.Appointment;
 
 @Repository("adminRepository")
-public interface AppointmentRepository extends JpaRepository<Admin, Long> {
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
-	Admin findByEmail(String user);
-	
-	List<Admin> findByRole(String user);
 }
