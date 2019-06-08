481b7e2e183d19f32b67af47413d16b5cb2041ed
soumyadip007 <soumyadip.note@gmail.com>
2019-06-08 15:41:16 +0530

    getter setter added appointment

--- bioMedical/src/main/java/com/spring/bioMedical/entity/Appointment.java
+++ bioMedical/src/main/java/com/spring/bioMedical/entity/Appointment.java
@@ -35,6 +35,66 @@ public class Appointment {
 	
 	@Column(name = "description")
 	private String description;
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+
+	public String getTime() {
+		return time;
+	}
+
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	
 	
 
