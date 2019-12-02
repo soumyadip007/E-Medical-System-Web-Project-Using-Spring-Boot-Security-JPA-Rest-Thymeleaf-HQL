92f02e8fbd353fb2e766131f4b0d617c6ff230eb
unknown <soumyadip.note@GMAIL.COM>
2019-12-02 13:45:55 +0530

    Conadded

---
+++ bioMedical/src/main/resources/static/fonts/ionicons/fonts/com/com/spring/bioMedical/entity/Admin.java
@@ -0,0 +1,157 @@
+package com.spring.bioMedical.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.data.annotation.Transient;
+
+/**
+ * 
+ * @author Soumyadip Chowdhury
+ * @github soumyadip007
+ *
+ */
+@Entity
+@Table(name = "user")
+public class Admin {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "username", nullable = false, unique = true)
+	@Email(message = "Please provide a valid e-mail")
+	@NotEmpty(message = "Please provide an e-mail")
+	private String email;
+	
+	@Column(name = "password")
+	@Transient
+	private String password;
+	
+	@Column(name = "first_name")
+	@NotEmpty(message = "Please provide your first name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	@NotEmpty(message = "Please provide your last name")
+	private String lastName;
+	
+	@Column(name = "enabled")
+	private boolean enabled;
+	
+	@Column(name = "confirmation_token")
+	private String confirmationToken;
+
+	@Column(name = "gender")
+	private String gender;
+	
+	
+	@Column(name = "authority")
+	private String role;
+	
+	@Column(name = "lastseen")
+	@Transient
+	private String lastseen;
+	
+	
+	
+	
+	public String getLastseen() {
+		return lastseen;
+	}
+
+	public void setLastseen(String lastseen) {
+		this.lastseen = lastseen;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	
+	public String getConfirmationToken() {
+		return confirmationToken;
+	}
+
+	public void setConfirmationToken(String confirmationToken) {
+		this.confirmationToken = confirmationToken;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean value) {
+		this.enabled = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [id=" + id + ", email=" + email + ", password=" + password + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", enabled=" + enabled + ", confirmationToken=" + confirmationToken
+				+ ", gender=" + gender + ", role=" + role + ", lastseen=" + lastseen + "]";
+	}
+
+	
+
+}
